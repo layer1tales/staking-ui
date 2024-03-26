7be4705fef607f42c942b0a3a32148ae578e62d1
gonwi <gonzalo.astorgaie@gmail.com>
2024-03-26 20:04:59 +0100

    feat: useStakePoolId useQuery v5 notation

--- hooks/useStakePoolId.tsx
+++ hooks/useStakePoolId.tsx
@@ -5,21 +5,20 @@ import { useRouter } from 'next/router'
 import { useStakePoolMetadataCtx } from 'providers/StakePoolMetadataProvider'
 
 export const useStakePoolId = () => {
-  const stakePoolMetadata = useStakePoolMetadataCtx()
-  const {
-    query: { stakePoolId },
-  } = useRouter()
+  const stakePoolMetadata = useStakePoolMetadataCtx();
+  const { query: { stakePoolId } } = useRouter();
 
-  return useQuery(
-    [
+  return useQuery({
+    queryKey: [
       'useStakePoolId',
       stakePoolId?.toString(),
       stakePoolMetadata.data?.stakePoolAddress.toString(),
     ],
-    async () => {
-      if (stakePoolMetadata.data)
-        return new PublicKey(stakePoolMetadata.data.stakePoolAddress)
-      return tryPublicKey(stakePoolId) ?? null
-    }
-  )
-}
+    queryFn: async () => {
+      if (stakePoolMetadata.data) {
+        return new PublicKey(stakePoolMetadata.data.stakePoolAddress);
+      } 
+      return tryPublicKey(stakePoolId) ?? null;
+    } 
+  });
+};
